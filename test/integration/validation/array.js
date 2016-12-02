ff6c7ef40ad4010a8916ce133606acc9a8fea3a2
colthreepv <mister.gamer@gmail.com>
2016-12-02 11:53:07 +0100

    test adapted to new context change

--- test/integration/validation/array.js
+++ test/integration/validation/array.js
@@ -1,9 +1,8 @@
-var Joi = require('joi');
-
+var Joi = require('joi')
 
 module.exports = {
   body: {
     numbers: Joi.array().items(Joi.number().valid([1, 2, 3, 4, 5])),
-    validate_numbers: Joi.array().items(Joi.number().valid(Joi.ref('$numbers')))
-  }
-};
+    validate_numbers: Joi.array().items(Joi.number().valid(Joi.ref('$body.numbers'))),
+  },
+}
